2797b517f03fca9e5e7435f805de41ad29a94f6e
Renato Marcandier <renato.marcandier@gmail.com>
2021-01-13 22:11:50 -0300

    add buildscpec.yaml

--- docker/hello.py
+++ docker/hello.py
@@ -3,7 +3,7 @@ import falcon
 class HelloResource(object):
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
-        resp.body = ("Hello, World!2")
+        resp.body = ("Hello, World!")
 
 class Page2Resource(object):
     def on_get(self, req, resp):
